76a8217af8f391d2363b93d62ecd466c6a0fe1de
Rene Morais <rene.douglas.bsi@gmail.com>
2024-07-18 16:05:15 -0300

    ajuste atividade

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,17 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * 1 -> Implemente a função de listar para que retorne todos os funcionários cadastrados.
+ *      Utilize o código abaixo (linhas 48:70), mas realize os ajustes necessários:
+ *          - Carregue os dados através do modelo Funcionario para, então, apresentá-los
+ *          - Corrija o problema no alinhamento das colunas do relatório.
+ *
+ * 2 -> Implemente a função de alterar/atualizar um funcionário.
+ *
+ * 3 -> Implemente a função de deletar um funcionário.
+ */
+
 public class Main {
     public static void main(String[] args) {
 
@@ -33,12 +44,13 @@ public class Main {
         //funcionarioDAO.inserir(funcionario);
 
         // LISTAR TODOS OS FUNCIONARIOS
-        try {
-            ps = con.prepareStatement("SELECT * FROM funcionarios");
-            ResultSet rs = ps.executeQuery();
+
+        try (PreparedStatement pstmt = con.prepareStatement("SELECT * FROM funcionarios")) {
+
+            ResultSet rs = pstmt.executeQuery();
             while(rs.next()) {
 
-                var dadosFuncionario = "ID: %s NOME: %s \t DATA NASC.: %s \t SALÁRIO: %s \t DT. REGISTRO: %s \t ATIVO: %s"
+                var dadosFuncionario = "ID: %s \t NOME: %s \t DATA NASC.: %s \t SALÁRIO: %s \t DT. REGISTRO: %s \t ATIVO: %s"
                         .formatted(
                                 rs.getInt("id"),
                                 rs.getString("nome"),
@@ -50,7 +62,7 @@ public class Main {
 
                 System.out.println(dadosFuncionario);
             }
-            ps.close();
+            pstmt.close();
             con.close();
 
         } catch (Exception e) {
